95531002f6fbef08fa6fad8dabbe9595d1ce3004
Jasper Rutte <jasperrutte@gmail.com>
2023-06-08 15:48:47 +0200

    finished assignment

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
 let animals = ["lion", "lobster", "seagull", "skunk", "turtle", "bear", "deer", "frog", "horse"];
 randomAnimals1 = shuffle([...animals]);
 randomAnimals2 = shuffle([...animals]);
+turns = 0
 
 twoCards = [];
+
 
 function shuffle(array) {
     let currentIndex = array.length,  randomIndex;
@@ -26,36 +28,54 @@ function turnAround(button){
 
 function sameCards (attribute) {
     twoCards.push(attribute.id)
-    console.log(twoCards[0])
-    console.log(twoCards[1])
-    document.getElementById(twoCards[0]).disabled = true;
-    if (twoCards.length > 1){
-        if (twoCards[0] !== twoCards[1]){
-            setTimeout(=>doc, 2000)
-            twoCards = [];
 
+    if (twoCards.length !== 2) {
+        document.getElementById(twoCards[0]).disabled = true
+    } else {
+        document.getElementById(twoCards[1]).disabled = true
+
+        if (twoCards[0].replace(/.$/,"") === twoCards[1].replace(/.$/,"")){
+            twoCards = []
+            return
+        }
+        setTimeout(turnCardsBack,1000, twoCards)
+        document.getElementById(twoCards[0]).disabled = false
+        document.getElementById(twoCards[1]).disabled = false
+        twoCards = []
+    }
+    let j = 0
+    let disabledButtons = document.getElementsByTagName("button");
+    for (let i = 0; i < disabledButtons.length; i++){
+        if (disabledButtons[i].disabled === true){
+            break
+        } else {
+            j++
+            if (j===disabledButtons.length){
+                alert("poggers")
+            }
         }
     }
+
 }
 
-function turnCardsBack(attribute){
+function turnCardsBack(twoCards){
     document.getElementById(twoCards[0]).style.backgroundImage = "url()";
     document.getElementById(twoCards[1]).style.backgroundImage = "url()";
 }
 
 
-for (let num = 0; num < animals.length-1; num++){
+for (let num = 0; num < animals.length; num++){
     let button = document.createElement("button");
     button.setAttribute("onClick", "turnAround(this);sameCards(this)")
-    // button.setAttribute("onClick", "sameCards(this)")
     button.setAttribute("id", randomAnimals1[num]+String(1))
     row1.appendChild(button);
 }
 
-for (let num = 1; num < animals.length; num++){
+for (let num = 0; num < animals.length; num++){
     let button = document.createElement("button");
-    button.setAttribute("onClick", "turnAround(this)")
-    button.setAttribute("onClick", "sameCards(this)")
-    button.setAttribute("id", randomAnimals1[num]+String(2))
+    button.setAttribute("onClick", "turnAround(this);sameCards(this)")
+    button.setAttribute("id", randomAnimals2[num]+String(2))
     row2.appendChild(button);
 }
+
+
